d0332fe1f23e789cf81250233db7366d3ce34f81
Chad Meyer <chadmeyer@me.com>
2014-08-16 22:18:59 -0500

    Adding a few more resource ways to pull data.

--- Restify/ApiSet.cs
+++ Restify/ApiSet.cs
@@ -4,6 +4,7 @@ using Restify.Exceptions;
 using Restify.Extensions;
 using RestSharp;
 using RestSharp.Authenticators;
+using System.IO;
 
 namespace Restify {
     public abstract class ApiSet<T> where T : new() {
@@ -12,6 +13,8 @@ namespace Restify {
         private readonly string _baseUrl;
         private readonly ContentType _contentType;
         private readonly IDictionary<string, string> _requestHeaders;
+
+        public string BaseUrl { get { return _baseUrl; } }
 
         /// <summary>
         /// The url for retrieving a specific entity. Call Get(string id) to use this property
@@ -151,9 +154,24 @@ namespace Restify {
             return list.Data;
         }
 
-        public virtual bool Create(byte[] stream, string url = "") {
+        public virtual IRestResponse Post(string url) {
+            var request = CreateRestRequest(Method.POST, url);
+
+            var client = new RestClient {
+                BaseUrl = _baseUrl
+            };
+
+            if (_ticket != null) {
+                client.Authenticator = OAuth1Authenticator.ForProtectedResource(_ticket.ConsumerKey, _ticket.ConsumerSecret, _ticket.AccessToken, _ticket.AccessTokenSecret);
+            }
+            var response = client.Execute(request);
+
+            return response;
+        }
+
+        public virtual bool Create(System.IO.Stream stream, string url = "", string fileParamaterName = "stream", string fileName = "") {
             var targetUrl = string.Empty;
-            
+
             if (!string.IsNullOrWhiteSpace(url)) {
                 if (url.Trim().Length <= _baseUrl.Length) {
                     throw new Exception("Invalid url: " + url);
@@ -169,9 +187,54 @@ namespace Restify {
                 targetUrl = CreateUrl;
             }
 
-            var request = CreateRestRequest(Method.POST, targetUrl);
-            request.AddFile("stream", stream, string.Empty);
+            var request = CreateRestRequest(Method.POST, targetUrl, "multipart/form-data");
+            request.AddFile(fileParamaterName, ReadToEnd(stream), fileName, GetMIMEType(fileName));
+            request.AlwaysMultipartFormData = true;
 
+            var client = new RestClient {
+                BaseUrl = _baseUrl
+            };
+
+            if (_ticket != null) {
+                client.Authenticator = OAuth1Authenticator.ForProtectedResource(_ticket.ConsumerKey, _ticket.ConsumerSecret, _ticket.AccessToken, _ticket.AccessTokenSecret);
+            }
+            var response = client.Execute(request);
+
+            //var item = ExecuteRequest(request);
+            return (int)response.StatusCode < 300;
+        }
+
+        public virtual bool Create<S>(S entity, string url = "") where S : new() {
+            var targetUrl = string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(url)) {
+                if (url.Trim().StartsWith(_baseUrl)) {
+                    if (url.Trim().Length <= _baseUrl.Length) {
+                        throw new Exception("Invalid url: " + url);
+                    }
+                    targetUrl = url.Substring(_baseUrl.Length);
+                }
+                else {
+                    targetUrl = url;
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(targetUrl)) {
+                if (string.IsNullOrWhiteSpace(CreateUrl)) {
+                    throw new NotImplementedException("The property CreateUrl has no value on the ApiSet.");
+                }
+
+                targetUrl = CreateUrl;
+            }
+            var request = CreateRestRequest(Method.POST, targetUrl);
+            request.Timeout = 20000;
+
+            if (_contentType == ContentType.XML) {
+                request.AddParameter("application/xml", entity.ToXml(), ParameterType.RequestBody);
+            }
+            else if (_contentType == ContentType.JSON) {
+                request.AddParameter("application/json", Newtonsoft.Json.JsonConvert.SerializeObject(entity), ParameterType.RequestBody);
+            }
             var item = ExecuteRequest(request);
             return (int)item.StatusCode < 300;
         }
@@ -200,8 +263,13 @@ namespace Restify {
             }
             var request = CreateRestRequest(Method.POST, targetUrl);
             request.Timeout = 20000;
-            request.AddParameter("application/xml", entity.ToXml(), ParameterType.RequestBody);
 
+            if (_contentType == ContentType.XML) {
+                request.AddParameter("application/xml", entity.ToXml(), ParameterType.RequestBody);
+            }
+            else if (_contentType == ContentType.JSON) {
+                request.AddParameter("application/json", Newtonsoft.Json.JsonConvert.SerializeObject(entity), ParameterType.RequestBody);
+            }
             var item = ExecuteRequest(request);
             return item.Data;
         }
@@ -321,14 +389,35 @@ namespace Restify {
 
             if ((int)response.StatusCode > 300) {
                 throw new ApiAccessException(response.StatusDescription) {
-                    StatusCode = response.StatusCode, 
-                    StatusDescription = response.StatusDescription, 
+                    StatusCode = response.StatusCode,
+                    StatusDescription = response.StatusDescription,
                     RequestUrl = response.ResponseUri.AbsoluteUri
                 };
             }
 
             if (!string.IsNullOrEmpty(response.ErrorMessage)) {
                 throw new ApiAccessException(response.ErrorMessage);
+            }
+
+            return response;
+        }
+
+        protected IRestResponse ExecuteGenericRequest(IRestRequest request) {
+            var client = new RestClient {
+                BaseUrl = _baseUrl
+            };
+
+            if (_ticket != null) {
+                client.Authenticator = OAuth1Authenticator.ForProtectedResource(_ticket.ConsumerKey, _ticket.ConsumerSecret, _ticket.AccessToken, _ticket.AccessTokenSecret);
+            }
+            var response = client.Execute(request);
+
+            if ((int)response.StatusCode > 300) {
+                throw new ApiAccessException(response.StatusDescription) {
+                    StatusCode = response.StatusCode,
+                    StatusDescription = response.StatusDescription,
+                    RequestUrl = response.ResponseUri.AbsoluteUri
+                };
             }
 
             return response;
@@ -346,8 +435,8 @@ namespace Restify {
 
             if ((int)response.StatusCode > 300) {
                 throw new ApiAccessException(response.StatusDescription) {
-                    StatusCode = response.StatusCode, 
-                    StatusDescription = response.StatusDescription, 
+                    StatusCode = response.StatusCode,
+                    StatusDescription = response.StatusDescription,
                     RequestUrl = response.ResponseUri.AbsoluteUri
                 };
             }
@@ -367,8 +456,8 @@ namespace Restify {
 
             if ((int)response.StatusCode > 300) {
                 throw new ApiAccessException(response.StatusDescription) {
-                    StatusCode = response.StatusCode, 
-                    StatusDescription = response.StatusDescription, 
+                    StatusCode = response.StatusCode,
+                    StatusDescription = response.StatusDescription,
                     RequestUrl = response.ResponseUri.AbsoluteUri
                 };
             }
@@ -376,14 +465,14 @@ namespace Restify {
             return response;
         }
 
-        private RestRequest CreateRestRequest(Method method, string url) {
+        private RestRequest CreateRestRequest(Method method, string url, string contentType = null) {
 
             var request = new RestRequest(method) {
                 Resource = url
             };
             request.RequestFormat = _contentType == ContentType.JSON ? DataFormat.Json : DataFormat.Xml;
             request.AddHeader("Accept-Encoding", "gzip,deflate");
-            request.AddHeader("Content-Type",  _contentType == ContentType.XML ? "application/xml" : "application/json");
+            request.AddHeader("Content-Type", !string.IsNullOrEmpty(contentType) ? contentType : _contentType == ContentType.XML ? "application/xml" : "application/json");
 
             if (_requestHeaders != null && _requestHeaders.Count > 0) {
                 foreach (var current in _requestHeaders) {
@@ -393,6 +482,247 @@ namespace Restify {
 
             return request;
         }
-        #endregion Private Methods 
+
+        //method for converting stream to byte[]
+        private byte[] ReadToEnd(System.IO.Stream stream) {
+            long originalPosition = stream.Position;
+            stream.Position = 0;
+
+            try {
+                byte[] readBuffer = new byte[4096];
+
+                int totalBytesRead = 0;
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(readBuffer, totalBytesRead, readBuffer.Length - totalBytesRead)) > 0) {
+                    totalBytesRead += bytesRead;
+
+                    if (totalBytesRead == readBuffer.Length) {
+                        int nextByte = stream.ReadByte();
+                        if (nextByte != -1) {
+                            byte[] temp = new byte[readBuffer.Length * 2];
+                            Buffer.BlockCopy(readBuffer, 0, temp, 0, readBuffer.Length);
+                            Buffer.SetByte(temp, totalBytesRead, (byte)nextByte);
+                            readBuffer = temp;
+                            totalBytesRead++;
+                        }
+                    }
+                }
+
+                byte[] buffer = readBuffer;
+                if (readBuffer.Length != totalBytesRead) {
+                    buffer = new byte[totalBytesRead];
+                    Buffer.BlockCopy(readBuffer, 0, buffer, 0, totalBytesRead);
+                }
+                return buffer;
+            }
+            finally {
+                stream.Position = originalPosition;
+            }
+        }
+
+        private readonly Dictionary<string, string> MIMETypesDictionary = new Dictionary<string, string>
+  {
+    {"ai", "application/postscript"},
+    {"aif", "audio/x-aiff"},
+    {"aifc", "audio/x-aiff"},
+    {"aiff", "audio/x-aiff"},
+    {"asc", "text/plain"},
+    {"atom", "application/atom+xml"},
+    {"au", "audio/basic"},
+    {"avi", "video/x-msvideo"},
+    {"bcpio", "application/x-bcpio"},
+    {"bin", "application/octet-stream"},
+    {"bmp", "image/bmp"},
+    {"cdf", "application/x-netcdf"},
+    {"cgm", "image/cgm"},
+    {"class", "application/octet-stream"},
+    {"cpio", "application/x-cpio"},
+    {"cpt", "application/mac-compactpro"},
+    {"csh", "application/x-csh"},
+    {"css", "text/css"},
+    {"dcr", "application/x-director"},
+    {"dif", "video/x-dv"},
+    {"dir", "application/x-director"},
+    {"djv", "image/vnd.djvu"},
+    {"djvu", "image/vnd.djvu"},
+    {"dll", "application/octet-stream"},
+    {"dmg", "application/octet-stream"},
+    {"dms", "application/octet-stream"},
+    {"doc", "application/msword"},
+    {"docx","application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+    {"dotx", "application/vnd.openxmlformats-officedocument.wordprocessingml.template"},
+    {"docm","application/vnd.ms-word.document.macroEnabled.12"},
+    {"dotm","application/vnd.ms-word.template.macroEnabled.12"},
+    {"dtd", "application/xml-dtd"},
+    {"dv", "video/x-dv"},
+    {"dvi", "application/x-dvi"},
+    {"dxr", "application/x-director"},
+    {"eps", "application/postscript"},
+    {"etx", "text/x-setext"},
+    {"exe", "application/octet-stream"},
+    {"ez", "application/andrew-inset"},
+    {"gif", "image/gif"},
+    {"gram", "application/srgs"},
+    {"grxml", "application/srgs+xml"},
+    {"gtar", "application/x-gtar"},
+    {"hdf", "application/x-hdf"},
+    {"hqx", "application/mac-binhex40"},
+    {"htm", "text/html"},
+    {"html", "text/html"},
+    {"ice", "x-conference/x-cooltalk"},
+    {"ico", "image/x-icon"},
+    {"ics", "text/calendar"},
+    {"ief", "image/ief"},
+    {"ifb", "text/calendar"},
+    {"iges", "model/iges"},
+    {"igs", "model/iges"},
+    {"jnlp", "application/x-java-jnlp-file"},
+    {"jp2", "image/jp2"},
+    {"jpe", "image/jpeg"},
+    {"jpeg", "image/jpeg"},
+    {"jpg", "image/jpeg"},
+    {"js", "application/x-javascript"},
+    {"kar", "audio/midi"},
+    {"latex", "application/x-latex"},
+    {"lha", "application/octet-stream"},
+    {"lzh", "application/octet-stream"},
+    {"m3u", "audio/x-mpegurl"},
+    {"m4a", "audio/mp4a-latm"},
+    {"m4b", "audio/mp4a-latm"},
+    {"m4p", "audio/mp4a-latm"},
+    {"m4u", "video/vnd.mpegurl"},
+    {"m4v", "video/x-m4v"},
+    {"mac", "image/x-macpaint"},
+    {"man", "application/x-troff-man"},
+    {"mathml", "application/mathml+xml"},
+    {"me", "application/x-troff-me"},
+    {"mesh", "model/mesh"},
+    {"mid", "audio/midi"},
+    {"midi", "audio/midi"},
+    {"mif", "application/vnd.mif"},
+    {"mov", "video/quicktime"},
+    {"movie", "video/x-sgi-movie"},
+    {"mp2", "audio/mpeg"},
+    {"mp3", "audio/mpeg"},
+    {"mp4", "video/mp4"},
+    {"mpe", "video/mpeg"},
+    {"mpeg", "video/mpeg"},
+    {"mpg", "video/mpeg"},
+    {"mpga", "audio/mpeg"},
+    {"ms", "application/x-troff-ms"},
+    {"msh", "model/mesh"},
+    {"mxu", "video/vnd.mpegurl"},
+    {"nc", "application/x-netcdf"},
+    {"oda", "application/oda"},
+    {"ogg", "application/ogg"},
+    {"pbm", "image/x-portable-bitmap"},
+    {"pct", "image/pict"},
+    {"pdb", "chemical/x-pdb"},
+    {"pdf", "application/pdf"},
+    {"pgm", "image/x-portable-graymap"},
+    {"pgn", "application/x-chess-pgn"},
+    {"pic", "image/pict"},
+    {"pict", "image/pict"},
+    {"png", "image/png"}, 
+    {"pnm", "image/x-portable-anymap"},
+    {"pnt", "image/x-macpaint"},
+    {"pntg", "image/x-macpaint"},
+    {"ppm", "image/x-portable-pixmap"},
+    {"ppt", "application/vnd.ms-powerpoint"},
+    {"pptx","application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+    {"potx","application/vnd.openxmlformats-officedocument.presentationml.template"},
+    {"ppsx","application/vnd.openxmlformats-officedocument.presentationml.slideshow"},
+    {"ppam","application/vnd.ms-powerpoint.addin.macroEnabled.12"},
+    {"pptm","application/vnd.ms-powerpoint.presentation.macroEnabled.12"},
+    {"potm","application/vnd.ms-powerpoint.template.macroEnabled.12"},
+    {"ppsm","application/vnd.ms-powerpoint.slideshow.macroEnabled.12"},
+    {"ps", "application/postscript"},
+    {"qt", "video/quicktime"},
+    {"qti", "image/x-quicktime"},
+    {"qtif", "image/x-quicktime"},
+    {"ra", "audio/x-pn-realaudio"},
+    {"ram", "audio/x-pn-realaudio"},
+    {"ras", "image/x-cmu-raster"},
+    {"rdf", "application/rdf+xml"},
+    {"rgb", "image/x-rgb"},
+    {"rm", "application/vnd.rn-realmedia"},
+    {"roff", "application/x-troff"},
+    {"rtf", "text/rtf"},
+    {"rtx", "text/richtext"},
+    {"sgm", "text/sgml"},
+    {"sgml", "text/sgml"},
+    {"sh", "application/x-sh"},
+    {"shar", "application/x-shar"},
+    {"silo", "model/mesh"},
+    {"sit", "application/x-stuffit"},
+    {"skd", "application/x-koan"},
+    {"skm", "application/x-koan"},
+    {"skp", "application/x-koan"},
+    {"skt", "application/x-koan"},
+    {"smi", "application/smil"},
+    {"smil", "application/smil"},
+    {"snd", "audio/basic"},
+    {"so", "application/octet-stream"},
+    {"spl", "application/x-futuresplash"},
+    {"src", "application/x-wais-source"},
+    {"sv4cpio", "application/x-sv4cpio"},
+    {"sv4crc", "application/x-sv4crc"},
+    {"svg", "image/svg+xml"},
+    {"swf", "application/x-shockwave-flash"},
+    {"t", "application/x-troff"},
+    {"tar", "application/x-tar"},
+    {"tcl", "application/x-tcl"},
+    {"tex", "application/x-tex"},
+    {"texi", "application/x-texinfo"},
+    {"texinfo", "application/x-texinfo"},
+    {"tif", "image/tiff"},
+    {"tiff", "image/tiff"},
+    {"tr", "application/x-troff"},
+    {"tsv", "text/tab-separated-values"},
+    {"txt", "text/plain"},
+    {"ustar", "application/x-ustar"},
+    {"vcd", "application/x-cdlink"},
+    {"vrml", "model/vrml"},
+    {"vxml", "application/voicexml+xml"},
+    {"wav", "audio/x-wav"},
+    {"wbmp", "image/vnd.wap.wbmp"},
+    {"wbmxl", "application/vnd.wap.wbxml"},
+    {"wml", "text/vnd.wap.wml"},
+    {"wmlc", "application/vnd.wap.wmlc"},
+    {"wmls", "text/vnd.wap.wmlscript"},
+    {"wmlsc", "application/vnd.wap.wmlscriptc"},
+    {"wrl", "model/vrml"},
+    {"xbm", "image/x-xbitmap"},
+    {"xht", "application/xhtml+xml"},
+    {"xhtml", "application/xhtml+xml"},
+    {"xls", "application/vnd.ms-excel"},                        
+    {"xml", "application/xml"},
+    {"xpm", "image/x-xpixmap"},
+    {"xsl", "application/xml"},
+    {"xlsx","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+    {"xltx","application/vnd.openxmlformats-officedocument.spreadsheetml.template"},
+    {"xlsm","application/vnd.ms-excel.sheet.macroEnabled.12"},
+    {"xltm","application/vnd.ms-excel.template.macroEnabled.12"},
+    {"xlam","application/vnd.ms-excel.addin.macroEnabled.12"},
+    {"xlsb","application/vnd.ms-excel.sheet.binary.macroEnabled.12"},
+    {"xslt", "application/xslt+xml"},
+    {"xul", "application/vnd.mozilla.xul+xml"},
+    {"xwd", "image/x-xwindowdump"},
+    {"xyz", "chemical/x-xyz"},
+    {"zip", "application/zip"}
+  };
+
+        private string GetMIMEType(string fileName) {
+            //get file extension
+            string extension = Path.GetExtension(fileName).ToLowerInvariant();
+
+            if (extension.Length > 0 &&
+                MIMETypesDictionary.ContainsKey(extension.Remove(0, 1))) {
+                return MIMETypesDictionary[extension.Remove(0, 1)];
+            }
+            return "unknown/unknown";
+        }
     }
+        #endregion Private Methods
 }
